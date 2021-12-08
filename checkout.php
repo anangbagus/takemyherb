baa5fde7bc29abb1321cbf5c7314dc61afb633c0
anangbagus <anangbagus666@gmail.com>
2021-12-08 19:03:59 +0800

    add checkout

--- checkout.php
+++ checkout.php
@@ -10,7 +10,9 @@
     };
 
     $username = $_SESSION['username'];
-    $keranjang = query("SELECT k.*, u.username, p.* FROM keranjang k INNER JOIN user u ON k.id_user = u.id_user INNER JOIN produk p ON k.id_produk = p.id_produk WHERE u.username = '$username'")
+    $keranjang = query("SELECT k.*, u.username, p.* FROM keranjang k INNER JOIN user u ON k.id_user = u.id_user INNER JOIN produk p ON k.id_produk = p.id_produk WHERE u.username = '$username'");
+    $jumlah = 1;
+    $total = 0;
 ?>
 
 <!DOCTYPE html>
@@ -19,10 +21,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Keranjang</title>
+    <title>Check Out</title>
 </head>
 <body>
-    <h1><ins>Keranjang</ins></h1>
+    <h1><ins>Check Out</ins></h1>
     <br>
 
     <table border="1" cellpadding="10" cellspacing="0">
@@ -30,23 +32,22 @@
             <th>No.</th>
             <th>Nama</th>
             <th>Harga</th>
-            <th>Deskripsi</th>
-            <th>Foto</th>
-            <th>Aksi</th>
+            <th>Jumlah</th>
+            <th>Total</th>
         </tr>
         <?php $i = 1; foreach($keranjang as $row) : ?>
         <tr>
             <td><?php echo $i; ?></td>
             <td><?=$row["nama_produk"]; ?></td>
             <td><?=$row["harga"]; ?></td>
-            <td><?=$row["deskripsi"]; ?></td>
-            <td><?=$row["foto"]; ?></td>
-            <td>
-                <a href="checkout.php?id_keranjang=<?= $row['id_keranjang'];?>">Check Out</a>
-                <a href="hapuskeranjang.php?id_keranjang=<?= $row['id_keranjang'];?>" onclick="return confirm('Hapus item ke keranjang?')">Hapus</a>
-            </td>
+            <td><?=$jumlah ?></td>
+            <td><?= ($row["harga"] * $jumlah); ?></td>
         </tr>
-        <?php $i++; endforeach; ?>
+        <?php $total+=($row["harga"] * $jumlah); $i++; endforeach; ?>
+        <tr>
+            <th colspan="4">Total</th>
+            <td><?= $total;?></td>
+        </tr>
     </table>
     <a href="">Check Out Semua</a>
 </body>
