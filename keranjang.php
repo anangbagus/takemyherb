908f3e497a07bb962746c4484e51013ee5fcb7e0
Ananta Wijaya <anantaw81@gmail.com>
2021-12-15 22:20:28 +0800

    add bootstrap

--- keranjang.php
+++ keranjang.php
@@ -19,14 +19,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <title>Keranjang</title>
 </head>
-<body>
-    <h1><ins>Keranjang</ins></h1>
+<body style="background-color: rgb(238,238,238);">
+    <?php include('_header.php'); ?>
+
+    <h1 class="text-center mt-3">Keranjang</h1>
     <br>
 
-    <table border="1" cellpadding="10" cellspacing="0">
+    
+
+    <!-- <table border="1" cellpadding="10" cellspacing="0">
         <tr>
             <th>No.</th>
             <th>Nama</th>
@@ -35,9 +42,38 @@
             <th>Foto</th>
             <th>Jumlah</th>
             <th>Aksi</th>
-        </tr>
+        </tr> -->
         <?php $i = 1; foreach($keranjang as $row) : ?>
-        <tr>
+        <form action="controlkeranjang.php" method="get">
+        <!--  -->
+        <div class="container mt-3 mb-3">
+            <div class="d-flex justify-content-center row">
+                <div class="col-md-10">
+                    <div class="row p-2 bg-white border rounded">
+                        <div class="col-md-2 mt-1 mr-3 text-end">
+                            <img class="img-fluid img-responsive rounded product-image" style="height: 160px;" src="img/<?=$row["foto"]; ?>.jpg"></div>
+                        <div class="col-md-6 mt-1">
+                            <h5><?=$row["nama_produk"]; ?></h5>
+                            <p class="text-justify para mb-0"> <?=$row["deskripsi"]; ?> <br><br></p>
+                        </div>
+                        <div class="align-items-center align-content-center col-md-4 border-left mt-1">
+                            <div class="d-flex flex-row align-items-center">
+                                <h4 class="mr-1">Rp.<?=$row["harga"]; ?>,00</h4><span class="strike-text"></span>
+                            </div>
+                            <div class="d-flex flex-column ">
+                                Jumlah: <input type="text" name="qty" id="qty" value="<?= $row["qty"];?>">
+                                <input type="hidden" name="id_keranjang" value="<?= $row['id_keranjang']?>">
+                                <input type="submit" class="btn btn-primary btn-sm mt-2" value="Checkout" name="checkout">
+                                <input type="submit" class="btn btn-outline-primary btn-sm mt-2" value="Hapus" name="hapusitem">    
+                            </div>  
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </form>
+        
+        <!-- <tr>
             <form action="controlkeranjang.php" method="get">
                 <td><?php echo $i; ?></td>
                 <td><?=$row["nama_produk"]; ?></td>
@@ -45,7 +81,7 @@
                 <td><?=$row["deskripsi"]; ?></td>
                 <td><?=$row["foto"]; ?></td>
                 <td>
-                    <input type="text" name="qty" id="qty" value="<?= $row["qty"];?>">
+                    <input type="text" name="qty" id="qty" value="<?= $row["stok"];?>">
                 </td>
                 <td>
                     <input type="hidden" name="id_keranjang" value="<?= $row['id_keranjang']?>">
@@ -53,8 +89,9 @@
                     <input type="submit" value="Hapus" name="hapusitem">
                 </td>
             </form>
-        </tr>
+        </tr> -->
         <?php $i++; endforeach; ?>
     </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
