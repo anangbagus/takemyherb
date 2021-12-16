9cd41598b90e097598ab66dd772d99220b7bcc44
Ananta Wijaya <anantaw81@gmail.com>
2021-12-16 08:46:00 +0800

    add sidebar dan bootstrap to detailpesanan

---
+++ _sidebar.php
@@ -0,0 +1,43 @@
+<div class="col-lg-3 col-md-4">
+    <!-- sidebar -->
+    <h1 class="visually-hidden">Sidebars examples</h1>
+
+    <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px;">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+        <i class="bi bi-person-circle" style="font-size: 30px;"></i> <h5 class="mt-3 ml-5">Admin</h5>
+    </a>
+    <hr>
+    <ul class="nav nav-pills flex-column mb-auto">
+        <!-- <li class="nav-item">
+        <a href="#" class="nav-link text-white" aria-current="page">
+            <i class="bi bi-house-door"></i> Home
+        </a>
+        </li> -->
+        <li>
+        <a href="produk.php" class="nav-link text-white">
+            <i class="bi bi-columns-gap"></i> Produk 
+        </a>                      
+        </li>
+        <li>
+        <a href="" class="nav-link text-white ">
+            <i class="bi bi-file-person"></i> Akun
+        </a>
+        </li>
+        <li>
+        <a href="detailpesanan.php" class="nav-link text-white">
+            <i class="bi bi-box-seam"></i> Pemesanan
+        </a>
+        </li>
+        <li>
+        <a href="logout.php" class="nav-link text-white">
+            <i class="bi bi-box-arrow-right"></i> Keluar
+        </a>
+        </li>
+    </ul>
+    <hr>
+    <div class="dropdown">
+    </div>
+    </div>
+
+<div class="b-example-divider"></div>
+</div>
--- detailpesanan.php
+++ detailpesanan.php
@@ -43,122 +43,73 @@
     <link rel="stylesheet" href="css/style.css">
     <title>produk</title>
 </head>
-<body>
-
+<body  style="background-color: rgb(238,238,238);">
     <div class="container-fluid py-5">
         <div class="container">
             <div class="col-12 mb-4">
                 <div class="row">
-                    <div class="col-lg-3 col-md-4">
-                        <!-- sidebar -->
-                        <h1 class="visually-hidden">Sidebars examples</h1>
+                    
+                    <?php include('_sidebar.php'); ?>
 
-                        <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px;">
-                        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                            <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-                            <span class="fs-4">Admin</span>
-                        </a>
-                        <hr>
-                        <ul class="nav nav-pills flex-column mb-auto">
-                            <li class="nav-item">
-                            <a href="#" class="nav-link text-white" aria-current="page">
-                                <i class="bi bi-house-door"></i> Home
-                            </a>
-                            </li>
-                            <li>
-                            <a href="#" class="nav-link active">
-                                <i class="bi bi-columns-gap"></i> Produk 
-                            </a>                      
-                            </li>
-                            <li>
-                            <a href="profile.php" class="nav-link text-white">
-                                <i class="bi bi-file-person"></i> Akun
-                            </a>
-                            </li>
-                            <li>
-                            <a href="#" class="nav-link text-white">
-                                <i class="bi bi-wallet2"></i> Metode Pembayaran
-                            </a>
-                            </li>
-                            <li>
-                            <a href="logout.php" class="nav-link text-white">
-                                <i class="bi bi-box-arrow-right"></i> Keluar
-                            </a>
-                            </li>
-                            
-                        </ul>
-                        <hr>
-                        <div class="dropdown">
-                            <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-                            <strong>mdo</strong>
-                            </a>
-                            <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-                            <li><a class="dropdown-item" href="#">New project...</a></li>
-                            <li><a class="dropdown-item" href="#">Settings</a></li>
-                            <li><a class="dropdown-item" href="#">Profile</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                            <li><a class="dropdown-item" href="#">Sign out</a></li>
-                            </ul>
-                        </div>
-                        </div>
+                    
 
-                    <div class="b-example-divider"></div>
-                    </div>
-
-
-                    <div class="col-9 col-md-8 ">
-                        <h1><ins>Detail Pesanan</ins></h1>
-                        <br> <br>
+                    <div class="col-lg-9 col-md-8 ">
+                        <h1 class="text-center">Detail Pesanan</h1>
+                        
                         <!-- menampilkan jumlah produk -->
                         <a>Jumlah Pesanan: <?php echo $jumlah["COUNT(*)"];?>
-                        <!-- menampilkan table -->
-                        <table border="1" cellpadding="10" cellspacing="0">
-                            <tr>
-                                <th>ID</th>
-                                <th>Tanggal Pemesanan</th>
-                                <th>Nama Pemesan</th>
-                                <th>Nama Produk</th>
-                                <th>Jumlah</th>
-                                <th>Total Harga</th>
-                                <th>Alamat</th>
-                                <th>Status</th>
-                                <th>Aksi</th>
-                            </tr>
-                            
-                            <?php foreach($pesanan as $row) : ?>
-                            <tr>
-                                <td><?php echo $row["id_pemesanan"]; ?></td>
-                                <td><?=$row["tgl_pemesanan"]; ?></td>
-                                <td><?=$row["id_user"]; ?></td>
-                                <td><?=$row["id_produk"]; ?></td>
-                                <td><?=$row["quantity"]; ?></td>
-                                <td><?=$row["total_harga"]; ?></td>
-                                <td><?=$row["alamat_pengiriman"]; ?></td>
-                                <td>
-                                    <?php
-                                    if($row["status"] == 0){
-                                        echo "Canceled";
-                                    }
-                                    else if($row["status"] == 1){
-                                        echo "Waiting";
-                                    }
-                                    else if($row["status"] == 2){
-                                        echo "On Process";
-                                    }
-                                    else if($row["status"] == 3){
-                                        echo "Success";
-                                    }
-                                    ?>
-                                </td>
-                                <td>
-                                    <a href="modelpesanan.php?id_pemesanan=<?=$row["id_pemesanan"]; ?>&mode=OnProc"><i class="bi bi-pencil-square"></i></a> |
-                                    <a href="modelpesanan.php?id_pemesanan=<?=$row["id_pemesanan"]; ?>&mode=Cancel" onclick="return confirm('yakin?');"><i class="bi bi-trash-fill"></i></a>
-                                </td>
-                            </tr>
-                            <?php endforeach; ?>
-                        </table>
-                        <br> <br>
+                        
+                        <div class="table-responsive bg-white border rounded"  style="background-color: #ffffff;">
+                            <table class="table table-responsive table-borderless p-2">
+                                <thead>
+                                    <tr class="bg-light">
+                                        <th scope="col" width="5%">#</th>
+                                        <th scope="col" width="15%">Date</th>
+                                        <th scope="col" width="15%">Nama Pemesan</th>
+                                        <th scope="col" width="15%">Nama Produk</th>
+                                        <th scope="col" width="5%">Jumlah</th>
+                                        <th scope="col" width="15%">Total Harga</th>
+                                        <th scope="col" width="15%">Alamat</th>
+                                        <th scope="col" width="5%">Status</th>
+                                        <th scope="col" class="text-end" width="10%"><span>Aksi</span></th>
+                                    </tr>
+                                </thead>
+                                <?php foreach($pesanan as $row) : ?>
+                                <tbody>
+                                    <tr>
+                                        <td><?php echo $row["id_pemesanan"]; ?></td>
+                                        <td><?=$row["tgl_pemesanan"]; ?></td>
+                                        <td><?=$row["id_user"]; ?></td>
+                                        <td><?=$row["id_produk"]; ?></td>
+                                        <td><?=$row["quantity"]; ?></td>
+                                        <td><?=$row["total_harga"]; ?></td>
+                                        <td><?=$row["alamat_pengiriman"]; ?></td>
+                                        <td>
+                                        <?php
+                                            if($row["status"] == 0){
+                                                echo "Canceled";
+                                            }
+                                            else if($row["status"] == 1){
+                                                echo "Waiting";
+                                            }
+                                            else if($row["status"] == 2){
+                                                echo "On Process";
+                                            }
+                                            else if($row["status"] == 3){
+                                                echo "Success";
+                                            }
+                                        ?>
+                                        </td>
+                                        <td class="text-end">
+                                            <a href="modelpesanan.php?id_pemesanan=<?=$row["id_pemesanan"]; ?>&mode=OnProc"><i class=" bi bi-check2-circle text-success"></i></a> |
+                                            <a href="modelpesanan.php?id_pemesanan=<?=$row["id_pemesanan"]; ?>&mode=Cancel" onclick="return confirm('yakin?');"><i class="bi bi-trash-fill text-success"></i></a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                                <?php endforeach; ?>
+                            </table>
+                        </div>
+                        <br> 
                     </div>
                 </div>
             </div>
