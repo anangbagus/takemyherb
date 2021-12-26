f7954e02787a0e431a3035616f01c34d25c26b4c
Ananta Wijaya <anantaw81@gmail.com>
2021-12-26 17:19:04 +0800

    add css to market

--- loadproduk.php
+++ loadproduk.php
@@ -5,63 +5,61 @@ require('config.php');
 $produk_types = query("SELECT tipe, COUNT(*) AS jumlah FROM produk GROUP BY tipe;");
 // inisialisasi nilai jumlah data
 $jumlah = query("SELECT COUNT(*) AS total FROM produk;")[0];
-
 ?>
 
-
-
 <!-- product list -->
-    <div class="py-5 container">
-        <div class="col-12 mb-4">
-            <div class="row">
-                <div class="col-lg-3 col-md-4">
-                    <div class="list-group">
-                        <a href="google.com" class="list-group-item d-flex justify-content-between align-items-center">
-                            Semua Produk
-                            <span class="badge bg-primary rounded-pill"><?= $jumlah['total']; ?></span>
-                        </a>
+    <div class="py-3 container col-12 mb-4">
+        <div class="row">
+            <div class="col-lg-3 col-md-4"> 
+                <div class="list-group">
+                    <a href="#" class="list-group-item d-flex justify-content-between align-items-center">
+                        Semua Produk
+                        <span class="badge bg-success rounded-pill"><?= $jumlah['total']; ?></span>
+                    </a>
 
-                        <?php foreach($produk_types as $produk_type) : ?>
-                        <a href="google.com" class="list-group-item d-flex justify-content-between align-items-center"><?=$produk_type["tipe"]; ?>
-                            <span class="badge bg-primary rounded-pill"><?=$produk_type["jumlah"]; ?></span>
-                        </a>
-                        <?php endforeach; ?>
-                    </div>
+                    <?php foreach($produk_types as $produk_type) : ?>
+                    <a href="#" class="list-group-item d-flex justify-content-between align-items-center"><?=$produk_type["tipe"]; ?>
+                        <span class="badge bg-success rounded-pill"><?=$produk_type["jumlah"]; ?></span>
+                    </a>
+                    <?php endforeach; ?>
                 </div>
-                <div class="col-lg-9 col-md-8 ">
-                    <div class="row">
+            </div>
+            <div class="col-lg-9 col-md-8 ">
+                <div class="row">
 
-                    <?php 
-                    $keyword = "";
-                    if(isset($_POST['keyword'])){
-                        $keyword = $_POST['keyword'];
-                    };
+                <?php 
+                $keyword = "";
+                if(isset($_POST['keyword'])){
+                    $keyword = $_POST['keyword'];
+                };
 
-                    // pengambilan data produk sesuai dengan keyword
-                    $produk = query("SELECT * FROM produk WHERE nama_produk LIKE '%$keyword%' OR manfaat LIKE '%$keyword%' OR deskripsi LIKE '%$keyword%'");
+                // pengambilan data produk sesuai dengan keyword
+                $produk = query("SELECT * FROM produk WHERE nama_produk LIKE '%$keyword%' OR manfaat LIKE '%$keyword%' OR deskripsi LIKE '%$keyword%'");
 
-                    foreach($produk as $row) : ?>
-                    <div class="col-lg-3 col-md-4 col-sm-6 mb-5">
-                        <a href="detailproduk.php?id_produk=<?=$row["id_produk"]?>" class="test">
-                        <div class="card">
-                            <img src="img/<?=$row["foto"]; ?>.jpg" class="card-img-top" style="height: 200px; object-fit: cover;"  alt="<?=$row["nama_produk"]; ?>">
-                            <div class="card-body">
-                                <h5 class="card-title"><?=$row["nama_produk"]; ?>
-                                    <span class="badge bg-success rectangle-pill"><?=$row["stok"]; ?></span>
-                                </h5> 
-                                <div class="text-truncate">
-                                    <?=$row["deskripsi"]; ?>
-                                </div>
-                                <p class="font-monospace text-primary "> Rp.<?=$row["harga"]; ?>,00</p>
-                                <a href="addkeranjang.php?id_produk=<?= $row['id_produk'];?>" onclick="return confirm('Masukkan item ke keranjang?')">
-                                    <button class="btn btn-success btn-sm mt-2" value="Checkout" name="checkout"> Masukkan ke Keranjang </button>
-                                </a>
+                foreach($produk as $row) : ?>
+                <div class="market col-lg-3 col-md-4 col-sm-6 mb-5">
+                    <!-- <a href="detailproduk.php?id_produk=<?=$row["id_produk"]?>" class=""> -->
+                    <div class="card">
+                        <img src="img/<?=$row["foto"]; ?>.jpg" class="card-img-top" style="height: 200px; object-fit: cover;"  alt="<?=$row["nama_produk"]; ?>">
+                        <div class="card-body">
+                            <span class="market--title card-title"><?=$row["nama_produk"]; ?> </span> 
+                                <!-- <span class="badge bg-success rectangle-pill"><?=$row["stok"]; ?></span> -->
+                                <span class="market--price"><b class="text-success"> Rp.<?=$row["harga"]; ?>,00 </b></span>
+                            <div class="mb-2 mt-2 market--desc text-truncate">
+                                <?=$row["deskripsi"]; ?>
                             </div>
-                            </div>
+                            
+                            <a class="market--text" href="addkeranjang.php?id_produk=<?= $row['id_produk'];?>" onclick="return confirm('Masukkan item ke keranjang?')">
+                                <button class="btn btn-success btn-sm mt-2" value="Checkout" name="checkout"> <i class="bi bi-cart-plus"></i>Tambah</button>
+                            </a>
+                            <a class="market--text" href="detailproduk.php?id_produk=<?=$row["id_produk"]?>" class="">
+                                <button class="btn bg-light mt-2" > <i class="bi bi-eye"></i>Lihat</button>
                             </a>
                         </div>
-                    <?php endforeach; ?>
+                        </div>
+                        </a>
                     </div>
+                <?php endforeach; ?>
                 </div>
             </div>
         </div>
--- market.php
+++ market.php
@@ -29,22 +29,25 @@
 <head>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/style.css?php echo time(); ?>">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <title>produk</title>
+    <title>Produk</title>
 </head>
 <body>
 
     <?php include('_header.php'); ?>
-    <div class="product--header container">
-        <div class="product--header--search">
-            <form class="">
-                <input type="text" placeholder="Cari produk..." class="" name="keyword" id="keyword" autofocus placeholder="Cari" autocomplete="off">
-                <!-- <input type="search" class="form-control" placeholder="Search..." aria-label="Search"> -->
-            </form>
-        </div>
-        <div class="product--header--title">
-            <h2 class=""> Produk Kami</h2>
+
+    <div class="col-12 product--header container">
+        <div class="row">
+            <div class="col-lg-4 product--header--search">
+                <form class="">
+                    <input type="text" placeholder="Cari produk..." class="market--search" name="keyword" id="keyword" autofocus placeholder="Cari" autocomplete="off">
+                    <!-- <input type="search" class="form-control" placeholder="Search..." aria-label="Search"> -->
+                </form>
+            </div>
+            <div class="col-lg-7 d-flex justify-content-center align-item-center product--header--title">
+                <h2 class="">Produk Kami</h2>
+            </div>
         </div>
     </div>
         
@@ -52,6 +55,7 @@
     <!-- Menampilkan produk -->
     <div id="loadproduct"></div>
     <br>
+
     <script>
         $(document).ready(function(){
 		load_data();
