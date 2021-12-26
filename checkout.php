003b33421b926de2f97b498d3e4f3cb8d955863e
Ananta Wijaya <anantaw81@gmail.com>
2021-12-26 18:41:25 +0800

    add style to checkout

--- checkout.php
+++ checkout.php
@@ -34,7 +34,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css?php echo time(); ?>">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <style type="text/css">
         .input-style {
@@ -47,10 +47,9 @@
   </style>
   <title>Check Out</title>
 </head>
-<body style="background-color: rgb(238,238,238);">
+<body class="body">
     <?php include('_header.php'); ?>
-    <h1 class="text-center mt-3">Check Out</h1>
-    <br>
+    <h1 class="checkout--title text-center mt-3">Check Out</h1>
 
     <!-- <table border="1" cellpadding="10" cellspacing="0"> -->
         <!-- <tr>
@@ -62,24 +61,38 @@
         </tr> -->
         <?php $i = 1; foreach($keranjang as $row) : ?>
 
-        <div class="container mt-3 mb-3">
+        <div class="checkout--body container mt-3 mb-3">
             <div class="d-flex justify-content-center row">
                 <div class="col-md-10">
                     <div class="row p-2 bg-white border rounded">
                         <div class="col-md-2 mt-1 mr-3 text-end">
-                            <img class="img-fluid img-responsive rounded product-image" style="height: 160px;" src="img/<?=$row["foto"]; ?>.jpg"></div>
+                            <img class="checkout--image" style="height: 160px;" src="img/<?=$row["foto"]; ?>.jpg"></div>
                         <div class="col-md-6 mt-1">
                             <h5><?=$row["nama_produk"]; ?></h5>
-                            <p class="text-justify para mb-0"> <?=$row["deskripsi"]; ?></p>
+                            <p class="checkout--desc"> <?=$row["deskripsi"]; ?></p>
                         </div>
                         <div class="align-items-center align-content-center col-md-4 border-left mt-1">
                             <div class="d-flex flex-row align-items-center">
                                 <span class="strike-text"></span>
                             </div>
-                            <div class="d-flex flex-column ">
-                                Jumlah : <?= $row["qty"];?> </br>
-                                Harga  : <?=$row["harga"]; ?>
-                                <p> Total Harga: <h5 class="mr-1">Rp.<?= ($row["harga"] * $row["qty"]); ?>,00</h5> </p>
+                            <div class="">
+                                <table>
+                                    <tr>
+                                        <td>Jumlah</td>
+                                        <td>:</td>
+                                        <td><?= $row["qty"];?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Harga</td>
+                                        <td>:</td>
+                                        <td>Rp.<?=$row["harga"]; ?>,00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Jumlah*Harga</td>
+                                        <td>:</td>
+                                        <td>Rp.<?= ($row["harga"] * $row["qty"]); ?>,00</td>
+                                    </tr>
+                                </table>
                             </div>  
                         </div>
                     </div>
@@ -103,29 +116,29 @@
             <div class="d-flex justify-content-center row">
                 <div class="col-md-10">
                     <div class="row p-2 bg-white border rounded">
-                        <div class="col-md-8 mt-1">
-                            <label>Alamat</label>
-                            <input class="input-style" type="text" name="alamat" id="alamat">
-                            <input type="hidden" name="id_keranjang" value="<?= $id_keranjang;?>">
-                            <input type="hidden" name="total" value="<?= $total;?>">
-                            <br>
-                            <input type="submit" class="btn btn-primary btn-sm mt-2 d-inline" value="Kirim" name="kirim">
-                        </div>
-                        <div class="align-items-center align-content-center col-md-4 border-left mt-1">
-                            <div class="d-flex flex-row align-items-center">
+                        <div class="col-md-8 mt-1 row">
+                            <div class="row">
+                                <div class="col-md-2 align-self-center">
+                                    <label>Alamat : </label>
+                                </div>
+                                <div class="col-md-10">
+                                    <input class="checkout--search" type="text" name="alamat" id="alamat">
+                                    <input type="hidden" name="id_keranjang" value="<?= $id_keranjang;?>">
+                                    <input type="hidden" name="total" value="<?= $total;?>">
+                                </div>
                             </div>
+                        </div>
+                        <div class="align-self-center col-md-3">
                             <div class="d-flex flex-column ">
                                 <h5> Total : Rp.<?= $total;?>,00 </h5>
                             </div>  
                         </div>
+                        <input type="submit" class=" checkout--button" value="Kirim" name="kirim"/>
                     </div>
                 </div>
             </div>
         </div>
-    </form>
-
-
-        
+    </form>    
     <!-- </table> -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
