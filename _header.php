ff887b4ee147fc02a4c163bb1d36c1e7c90fb977
Ananta Wijaya <anantaw81@gmail.com>
2021-12-29 03:16:48 +0800

    remove comment

--- _header.php
+++ _header.php
@@ -2,7 +2,7 @@
     <div class="px-3 py-2 bg-success text-white">
     <div class="container">
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
+        <a href="market.php" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
             <i class="bi bi-flower3"></i>TakeMyHerb
         </a>
 
--- market.php
+++ market.php
@@ -40,12 +40,6 @@
 <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
 <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
 
-<script>
-  $(function() {
-    
-  } );
-</script>
-
 </head>
 <body>
 
